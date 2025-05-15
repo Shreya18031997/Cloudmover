227899b0105a326d6fafe1a942249016e0437446
Shreya Kumari <shreyakumari@Shreyas-MacBook-Air-2.local>
2025-05-14 20:21:34 -0700

    new changes

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -4,10 +4,11 @@ from fastapi.responses import RedirectResponse
 from google_auth_oauthlib.flow import Flow
 from app.config import CREDENTIALS_PATH, SCOPES, REDIRECT_URI
 import os
+from fastapi import Request
+from fastapi.responses import HTMLResponse
 
 router = APIRouter()
 
-# Allow non-HTTPS for testing locally
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 @router.get("/auth/google")
@@ -17,11 +18,9 @@ def login_via_google():
         scopes=SCOPES,
         redirect_uri=REDIRECT_URI
     )
-
     auth_url, _ = flow.authorization_url(prompt='consent')
     return RedirectResponse(auth_url)
 
-
 @router.get("/auth/callback")
 def google_callback(request: Request):
-    return {"message": "Google login successful! (you'll process tokens here later)"}
+    return HTMLResponse(content="<h1>✅ Login Successful!</h1><p>You can now access your Drive files.</p>")
